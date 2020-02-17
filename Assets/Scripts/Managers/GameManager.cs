95073bf338ea74eb50162ca6a700b529894fa75b
citi <mail>
2020-02-17 12:32:27 +0100

    02/24/20 - Card, basic layout and GameManager singleton

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Managers {
+    public class GameManager : MonoBehaviour {
+        public static GameManager Instance { get; private set; }
+
+        private void Awake() {
+            if (Instance != null && Instance != this) {
+                Destroy(gameObject);
+            }
+            else {
+                Instance = this;
+            }
+        }
+    }
+}
